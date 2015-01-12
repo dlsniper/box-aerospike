df4641ca7be663dc859e1e83217187714b1f143d
Florin Patan <florinpatan@gmail.com>
2015-01-12 16:07:32 +0100

    Fix install script

--- aerospike.sh
+++ aerospike.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget -O aerospike.tgz 'http://www.aerospike.com/artifacts/aerospike-server-community/3.4.0/aerospike-server-community-3.4.0-ubuntu12.04.tgz'
 tar -xvf aerospike.tgz
-cd aerospike-server-community-*-ubuntu12
+cd aerospike-server-community-*-ubuntu12.04
 sudo ./asinstall
 sudo service aerospike start
